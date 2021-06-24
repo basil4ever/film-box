0ebba7a400d6db131cbd586e04cd87883d3e3a86
basil4ever <basil4ever@mail.ru>
2021-06-24 18:13:30 +0300

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ActorsComponent} from "./actors/actors.component";
+import {GenresComponent} from "./genres/genres.component";
+import {FilmsComponent} from "./films/films.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "actors",
+    component: ActorsComponent
+  },
+  {
+    path: "films",
+    component: FilmsComponent
+  },
+  {
+    path: "genres",
+    component: GenresComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
