ffca5fad9e651dbd67e5dc10913131c8f0ba5c6e
basil4ever <basil4ever@mail.ru>
2021-06-28 12:18:18 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarModule } from 'ng-sidebar';
 import { ActorsComponent } from './actors/actors.component';
 import { GenresComponent } from './genres/genres.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +18,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     ActorsComponent,
     GenresComponent,
     SidebarComponent
+
   ],
   imports: [
     BrowserModule,
