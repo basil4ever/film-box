f45f89139a406a5e54cf363796e60d911f474eb8
basil4ever <basil4ever@mail.ru>
2021-06-28 10:29:32 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ActorsComponent,
-    GenresComponent
+    GenresComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
